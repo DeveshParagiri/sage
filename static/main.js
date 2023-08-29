ca40938551665b851c3a308b43e4825dad0d1409
DeveshParagiri <devesh.paragiri@gmail.com>
2023-08-29 17:41:01 +0530

    Basic UI developed.

---
+++ static/main.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+    $("#messageArea").on("submit", function(event) {
+        const date = new Date()
+        const hour = date.getHours();
+        const minute = (date.getMinutes()<10?'0':'') + date.getMinutes()
+        const str_time = hour+":"+minute;
+
+        var rawText = $("#text").val();
+
+        var userHtml = '<div class="d-flex justify-content-end mb-4"><div class="msg_container_send">' + rawText + '<span class="msg_time_send">'+ str_time + '</span></div>';
+        
+        $("#text").val("");
+        $("#messageFormeight").append(userHtml);
+
+        $.ajax({
+            data: {
+                msg: rawText,	
+            },
+            type: "POST",
+            url: "/get",
+        }).done(function(data) {
+            var botHtml = '<div class="d-flex justify-content-start mb-4"><div class="msg_container">' + data + '<span class="msg_time">' + str_time + '</span></div></div>';
+            $("#messageFormeight").append($.parseHTML(botHtml));
+        });
+        event.preventDefault();
+    });
+});
