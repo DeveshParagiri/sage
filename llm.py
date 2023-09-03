75f5cde5b4e1e0da6025b3dbf7d115aefbc5df0e
DeveshParagiri <devesh.paragiri@gmail.com>
2023-09-03 18:54:52 +0530

    LLM params changed.

--- llm.py
+++ llm.py
@@ -1,7 +1,10 @@
+# ================================================================================
+# This script instantiates the llm object with the relevant parameters
+# ================================================================================
 from langchain.llms import CTransformers
 
 # Local CTransformers wrapper for Llama-2-7B-Chat
-llm = CTransformers(model='/Users/deveshparagiri/Downloads/llama-2-7b-chat.ggmlv3.q8_0.bin', # Location of downloaded GGML model
+llm = CTransformers(model='/Users/deveshparagiri/Downloads/models/sage-v2-q8_0.bin',
                     model_type='llama', # Model type Llama
                     config={'max_new_tokens': 256,
-                            'temperature': 0.01})
+                            'temperature': 0.5})
